9791e5b2cb42cc62532cbb164fe94c8686e44688
berylatieno30 <berylatieno30@gmail.com>
2024-11-11 13:03:49 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// 1. set up a flag for the csv file
+// 2. read and parse the file
+// 3. Define quiz logic
+// 4. Display results
